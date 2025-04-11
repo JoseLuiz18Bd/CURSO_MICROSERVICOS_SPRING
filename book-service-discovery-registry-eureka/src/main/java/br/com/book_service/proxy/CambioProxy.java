1d8937467b8f5bd1c2502d4ccaa52b2977bdfad8
José Luiz <zeluizjsantos@gmail.com>
2025-04-11 15:22:57 -0300

    configurando uma api-gateway

--- book-service-discovery-registry-eureka/src/main/java/br/com/book_service/proxy/CambioProxy.java
+++ book-service-discovery-registry-eureka/src/main/java/br/com/book_service/proxy/CambioProxy.java
@@ -8,7 +8,7 @@ import br.com.book_service.response.Cambio;
 
 
 
-@FeignClient( name = "cambio-service", url = "localhost:8000")
+@FeignClient( name = "cambio-service")
 public interface CambioProxy {
 	
 	@GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
